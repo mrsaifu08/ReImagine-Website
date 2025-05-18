c762f2c68aad7988e2758696f1eb5e75eda4bdda
saifstack25@gmail.com <saifstack25@gmail.com>
2025-05-19 02:15:07 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,22 @@
+
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal');
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 150;
+
+    if (elementTop < windowHeight - revealPoint) {
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
+window.addEventListener('scroll', reveal);
+window.addEventListener('load', reveal);
+
+
+
